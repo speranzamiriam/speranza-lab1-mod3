47182090cd1201b11d87d4d889231879fd7f2db0
speranzamiriam <speranzamiriam1@gmail.com>
2023-02-27 22:43:59 -0400

    Codigo Lab1

---
+++ 04-lab1-tasks/tests/task.test.ts
@@ -0,0 +1,17 @@
+test('task 3', () => {
+    //TDD 
+    const input = 'HeFlo';
+    const task3 = jest.fn((input: string) => ['H', 'e', 'F', 'l', 'o']);
+    //const calc = new Calculadora();
+
+    //actual
+    const actual = task3(input);
+    
+
+    //expected ffff
+    const expc = ['H', 'e', 'F', 'l', 'o'];
+
+
+    expect(actual).toEqual(expc);
+    //expect(actual).toBeTruthy()
+});
